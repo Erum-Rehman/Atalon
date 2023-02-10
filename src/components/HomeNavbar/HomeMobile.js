83d4fc589b37ab3f7a695f3e3fff031fb92d6714
Erum-Rehman <erumr0443@gmail.com>
2023-02-10 22:31:37 +0500

    set Navbar

--- src/components/HomeNavbar/HomeMobile.js
+++ src/components/HomeNavbar/HomeMobile.js
@@ -16,14 +16,16 @@ const MobileNavbar = () => {
     return (
         <>
             <div className='secHomeNavbar'>
-            <Link to="/"><img alt="" className='pnav_m' src={`/assets/mobNav.png`} /></Link>
-
+            <div style={{ position: 'absolute' }}>
+                    <Link to="/"><img alt="" className='whiteNavbar_m' src={`/assets/mobLogo.png`} /></Link>
+                </div>
+                <img alt="" className='pnav_m' src={`/assets/mobileNav.png`} />
             <div className='list_m'>
                 <ul className="nav-items_m">
                     <li><Link to="/" className={pathname === '/' ? 'active' : ''}><img src={`/assets/home.png`}/></Link></li>
-                    <li><Link to="/company" className={pathname === '/Company' ? 'active' : ''}><img src={`/assets/co.png`}/></Link></li>
-                    <li><Link to="/business" className={pathname === '/Business' ? 'active' : ''}><img src={`/assets/business.png`}/></Link></li>
-                    <li><Link to="/news" className={pathname === '/News' ? 'active' : ''}><img src={`/assets/newsIcon.png`}/></Link></li>
+                    <li><Link to="/company" className={pathname === '/company' ? 'active' : ''}><img src={`/assets/co.png`}/></Link></li>
+                    <li><Link to="/business" className={pathname === '/business' ? 'active' : ''}><img src={`/assets/business.png`}/></Link></li>
+                    <li><Link to="/news" className={pathname === '/news' ? 'active' : ''}><img src={`/assets/newsIcon.png`}/></Link></li>
                     <li ><Link to="/contact" className={pathname === '/contact' ? 'active' : ''}><img src={`/assets/contact.png`}/></Link> </li>
                 </ul>
             </div>
--- src/components/Navbar/ScreenNavbar.js
+++ src/components/Navbar/ScreenNavbar.js
@@ -20,15 +20,18 @@ const ScreenNavbar = () => {
             </div>
 
             <div className='secNavbar'>
-            <Link to="/"><img alt="" className='pnav' src={`/assets/nav1.png`} /></Link>
+            <div style={{ position: 'absolute' }}>
+                    <Link to="/"><img alt="" className='whiteNavbar' src={`/assets/webLogo.png`} /></Link>
+                </div>
+                <img alt="" className='pnav' src={`/assets/whiteNav.png`} />
 
             <div className='list'>
                 <ul className="nav-items">
                     <li><Link to="/" className={pathname === '/' ? 'active' : ''}>Home</Link></li>
                     <li><Link to="/about" className={pathname === '/about' ? 'active' : ''}>About</Link></li>
-                    <li><Link to="/company" className={pathname === '/Company' ? 'active' : ''}>Company Information</Link></li>
-                    <li><Link to="/business" className={pathname === '/Business' ? 'active' : ''}>Business Overview</Link></li>
-                    <li><Link to="/news" className={pathname === '/News' ? 'active' : ''}>News 2022</Link></li>
+                    <li><Link to="/company" className={pathname === '/company' ? 'active' : ''}>Company Information</Link></li>
+                    <li><Link to="/business" className={pathname === '/business' ? 'active' : ''}>Business Overview</Link></li>
+                    <li><Link to="/news" className={pathname === '/news' ? 'active' : ''}>News 2022</Link></li>
                     <li ><Link to="/contact" className={pathname === '/contact' ? 'active' : ''}>Contact</Link> </li>
                 </ul>
             </div>
